809c4ad6ab3bc534ea776f89cce60f0796b07cf2
newsocialsandra <sandra.e.b.svensson@gmail.com>
2025-10-15 21:05:47 +0200

    Added functions
    - Function to create p-tags in the div
    - Eventlistener for reset button
    - For loop to reset all form values on reset button click

--- js/app.js
+++ js/app.js
@@ -1,6 +1,39 @@
 
-// Creating a variable for the form
+// Creating a variable for the form we want to get values from
 const form = document.getElementById("infoForm");
+
+// Creating a variable for the div we want to place new text in
+let resultDiv = document.getElementById("result");
+
+// Creating a function that creates an h2-tag in the div
+function createH2Tag(stringIn) {
+  let para = document.createElement("h2");
+  para.textContent = stringIn;
+  resultDiv.appendChild(para);
+}
+
+// Creating a function that creates p-tags in the div
+function createPTag(stringIn) {
+  let para = document.createElement("p");
+  para.textContent = stringIn;
+  resultDiv.appendChild(para);
+}
+
+// Creating a variable for the reset button
+const resetButton = document.getElementById("reset");
+
+// Creating a function that removes all values from form fields when pressing reset button
+
+const idList = ["fname", "lname", "adjective", "animal", "occupation", "subject", "fruit"];
+
+
+resetButton.addEventListener("click", () => {
+  for (let i = 0; i < idList.length; i++) {
+    resetValue = document.getElementById("lname");
+    resetValue.value = resetValue.innerHTML;
+  }
+})
+
 
 // Adding event listener for submit button
 form.addEventListener("submit", (event) => {
@@ -22,13 +55,17 @@ form.addEventListener("submit", (event) => {
 
       console.log(formObject);
       let pTag1 =`I’m ${formObject.lname}, ${formObject.fname} ${formObject.lname}. But you can call me Baby ${formObject.fname.slice(0,3)}. Yeah, I’m ${formObject.adjective.toLowerCase()} like that ;)`;
-      console.log(pTag1);
       let pTag2 = `What am I looking for in a partner? Well, like most people, I’d like to meet someone who's a ${formObject.animal.toLowerCase()} in the streets and a ${formObject.occupation.toLowerCase()} in the sheets.`;
-      console.log(pTag2);
-      let pTag3 = `Maybe you could teach me ${formObject.subject.toLowerCase()}, maybe while wearing nothing but a ${formObject.fruit.toLowerCase()} shaped hat.`;
-      console.log(pTag3);
+      let pTag3 = `Maybe you could teach me ${formObject.subject.toLowerCase()}, maybe while wearing nothing but a ${formObject.fruit.toLowerCase()} shaped hat ;)`;
       let pTag4 = `But let’s start small, huh? Hmu if you want to get a coffee or something.`;
-      console.log(pTag4);
+
+      createH2Tag("Behold! Your new Tinder bio:");
+      createPTag(pTag1);
+      createPTag(pTag2);
+      createPTag(pTag3);
+      createPTag(pTag4);
+
+
 });
 
 /*
